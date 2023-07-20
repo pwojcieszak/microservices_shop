c0a1c52bb2a3e81011b507716ff52fa5c2bd4452
pwojcieszak <piotrekw1002@gmail.com>
2023-07-20 23:54:01 +0200

    Added controller with endpoint to place order

---
+++ order-service/src/main/java/com/pwojcieszak/orderservice/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.pwojcieszak.orderservice.controller;
+
+import com.pwojcieszak.orderservice.dto.OrderRequest;
+import com.pwojcieszak.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order placed Successfully";
+    }
+}
