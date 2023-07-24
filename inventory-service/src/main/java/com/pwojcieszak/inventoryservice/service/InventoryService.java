de2190874e25aa5aa7b38d3c6769c485632c33e8
pwojcieszak <piotrekw1002@gmail.com>
2023-07-24 21:31:29 +0200

    Inventory service now checks returns DTO with a field informing of product's availability

--- inventory-service/src/main/java/com/pwojcieszak/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/pwojcieszak/inventoryservice/service/InventoryService.java
@@ -1,16 +1,26 @@
 package com.pwojcieszak.inventoryservice.service;
 
 import com.pwojcieszak.inventoryservice.repository.InventoryRepository;
+import dto.InventoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                )
+                .toList();
     }
 }
