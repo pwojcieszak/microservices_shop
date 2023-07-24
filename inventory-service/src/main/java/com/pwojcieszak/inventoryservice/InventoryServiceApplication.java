01f84add01c8a671dd7dcd0c364e5f9aa969d5d3
pwojcieszak <piotrekw1002@gmail.com>
2023-07-24 21:32:17 +0200

    Added bean with sample data for InventoryService table

--- inventory-service/src/main/java/com/pwojcieszak/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/pwojcieszak/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,12 @@
 package com.pwojcieszak.inventoryservice;
 
+import com.pwojcieszak.inventoryservice.model.Inventory;
+import com.pwojcieszak.inventoryservice.repository.InventoryRepository;
+import dto.InventoryResponse;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +15,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("Samsung");
+			inventory.setQuantity(10);
+
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("Iphone");
+			inventory1.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory1);
+		};
+	}
 }
