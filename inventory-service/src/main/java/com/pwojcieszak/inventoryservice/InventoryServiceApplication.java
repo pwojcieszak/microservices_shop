0ac27054c96a0504815c71aaaaeb2b8d5150f43b
pwojcieszak <piotrekw1002@gmail.com>
2023-07-25 21:42:36 +0200

    Added Eureka Service Discovery dependencies and annotations

--- inventory-service/src/main/java/com/pwojcieszak/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/pwojcieszak/inventoryservice/InventoryServiceApplication.java
@@ -2,13 +2,14 @@ package com.pwojcieszak.inventoryservice;
 
 import com.pwojcieszak.inventoryservice.model.Inventory;
 import com.pwojcieszak.inventoryservice.repository.InventoryRepository;
-import dto.InventoryResponse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/com/pwojcieszak/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/pwojcieszak/orderservice/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.pwojcieszak.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
