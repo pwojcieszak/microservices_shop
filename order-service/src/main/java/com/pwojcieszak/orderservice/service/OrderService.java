ff9511e531c12cafd8e9072e04e60377d989fab5
pwojcieszak <piotrekw1002@gmail.com>
2023-07-25 21:46:37 +0200

    Changed URI and WebClient to get access port from Eureka

--- order-service/src/main/java/com/pwojcieszak/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/pwojcieszak/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -37,8 +37,8 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        Flux<InventoryResponse> inventoryResponseFlux = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        Flux<InventoryResponse> inventoryResponseFlux = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToFlux(InventoryResponse.class);
