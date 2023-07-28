dfcf62280e674d71ed59882677c5bd7532753dcb
pwojcieszak <piotrekw1002@gmail.com>
2023-07-28 17:41:48 +0200

    Added sleep to test Timeout and Retry

--- inventory-service/src/main/java/com/pwojcieszak/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/pwojcieszak/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.pwojcieszak.inventoryservice.service;
 import com.pwojcieszak.inventoryservice.repository.InventoryRepository;
 import com.pwojcieszak.inventoryservice.dto.InventoryResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,10 +12,15 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
