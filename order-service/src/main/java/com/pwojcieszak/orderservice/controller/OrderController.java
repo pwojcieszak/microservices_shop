9b8033791a4ce4b5bdb84d1ad9f956fabe55a0c4
pwojcieszak <piotrekw1002@gmail.com>
2023-07-28 17:38:03 +0200

    Added Resilience annotations to controller. Added fallback method

--- order-service/src/main/java/com/pwojcieszak/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/pwojcieszak/orderservice/controller/OrderController.java
@@ -2,9 +2,14 @@ package com.pwojcieszak.orderservice.controller;
 
 import com.pwojcieszak.orderservice.dto.OrderRequest;
 import com.pwojcieszak.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -13,8 +18,15 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order placed Successfully";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        //TODO make method synchronous without using block() which generates an error. Return htpp error
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+       return CompletableFuture.supplyAsync(() -> "Something went wrong, order not placed");
     }
 }
